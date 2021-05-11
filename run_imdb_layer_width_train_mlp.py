4c7a830b47ff47c35c044384b9f9bc2f155ee97b
LeavesLei <leishiye@gmail.com>
2021-05-11 14:56:56 +0800

    m

--- run_imdb_layer_width_train_mlp.py
+++ run_imdb_layer_width_train_mlp.py
@@ -24,7 +24,7 @@ num_classes = 2
 weight_decay = 1e-6
 lr = 1e-2
 
-width_list = [3, 7, 10, 20, 30, 50, 60, 80, 100, 120, 150, 170, 200]
+width_list = [10, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
 
 # Load data
 ##########################################
@@ -52,7 +52,7 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 ###########################################
 
-
+"""
 num_train = int(x_train.shape[0] * 0.8)
 num_val = x_train.shape[0] - num_train
 mask = list(range(num_train, num_train+num_val))
@@ -62,7 +62,7 @@ y_val = y_train[mask]
 mask = list(range(num_train))
 x_train = x_train[mask]
 y_train = y_train[mask]
-
+"""
 input_shape = x_train.shape[1:]
 
 print('dataset: ' + dataset)
@@ -79,16 +79,8 @@ for iter in np.linspace(begin_repeat-1, begin_repeat + repeat-2, repeat).astype(
         mlp.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
         mlp.summary()
 
-        # Training policies
-        def lr_scheduler(epoch):
-            return lr * (0.1 ** (epoch // 20))
-
-        reduce_lr = LearningRateScheduler(lr_scheduler)
-        early_stopping = EarlyStopping(monitor='loss', patience=10)
-
         # training networks
-        mlp.fit(x_train, y_train, batch_size=batch_size, epochs=epoch, callbacks=[reduce_lr, early_stopping],
-                validation_data=(x_val, y_val), verbose=1)
+        mlp.fit(x_train, y_train, batch_size=batch_size, epochs=epoch, verbose=1)
 
         mlp.save(save_path + str(num_neuron) + '_' + dataset + '_depth_' +str(depth)  + '_iter' + str(iter + 1) + '.h5')
 
