20575c512c0bde35229e13e91a9d83c15e932d72
LeavesLei <leishiye@gmail.com>
2021-05-13 16:48:01 +0800

    m

---
+++ TinyImagenet/script_ti_tt_5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# SBATCH -A test
+#SBATCH -J ti_ss_5
+#SBATCH -p nips
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
+#SBATCH -o ti_ss_5.out
+#SBATCH -t 10:00:00
+
+cd /public/data1/users/leishiye
+source .bashrc
+cd neural_code/activation-code/TinyImagenet
+
+python run_tinyimagenet_sample_size_train_cnn.py --begin_repeat 5
