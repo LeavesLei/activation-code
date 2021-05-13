8de667f3ce5794225bf65e864743f24da4be9d47
LeavesLei <leishiye@gmail.com>
2021-05-13 22:46:50 +0800

    m

--- TinyImagenet/run_tinyimagenet_sample_size_encoding_properties.py
+++ TinyImagenet/run_tinyimagenet_sample_size_encoding_properties.py
@@ -100,6 +100,9 @@ for iter in np.linspace(begin_repeat-1, begin_repeat + repeat-2, repeat).astype(
             image_datasets['train'] = TinyImageNet(data_dir, train=True, transform=data_transforms['train'], sample_size=sample_size)
             image_datasets['test'] = TinyImageNet(data_dir, train=False, transform=data_transforms['test'])
             dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=128, shuffle=True, num_workers=4) for x in ['train', 'test']}
+            trainloader = dataloaders['train']
+            testloader = dataloaders['test']
+            
             dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
 
             net = torch.load(load_path + str(sample_size) + '_width_' + str(num_neuron) + '_' +
