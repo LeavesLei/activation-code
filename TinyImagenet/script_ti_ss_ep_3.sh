947458d0b761d1cfc4a855df50a4872249fb3808
LeavesLei <leishiye@gmail.com>
2021-05-14 23:33:57 +0800

    m

---
+++ TinyImagenet/script_ti_ss_ep_3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# SBATCH -A test
+#SBATCH -J ti_ss_ep_3
+#SBATCH -p nips
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
+#SBATCH -o ti_ss_ep_3.out
+#SBATCH -t 10:00:00
+
+cd /public/data1/users/leishiye
+source .bashrc
+cd neural_code/activation-code/TinyImagenet
+
+python run_tinyimagenet_sample_size_encoding_properties.py --begin_repeat 3
